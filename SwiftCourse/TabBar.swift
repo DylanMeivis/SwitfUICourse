72aba1e8a3fe73473280a0c6447f0a10e199c9ca
Dylan Meivis <dylandiamonds@live.nl>
2020-03-25 18:08:12 +0100

    Section 20 📓

---
+++ SwiftCourse/TabBar.swift
@@ -0,0 +1,31 @@
+//
+//  TabBar.swift
+//  SwiftCourse
+//
+//  Created by Dylan Meivis on 25/03/2020.
+//  Copyright © 2020 Dylan Meivis. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Certificates")
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar().previewDevice("iPhone 8")
+    }
+}
